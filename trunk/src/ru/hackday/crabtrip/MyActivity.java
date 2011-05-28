b569d4ee11b42075c14088766a8e8ffb5c1982d4
jecklandin@gmail.com <jecklandin@gmail.com@a6d49d11-235e-086e-d3b0-479171f3bf0b>
2011-05-28 16:46:43 +0000

    init sound manager

--- trunk/src/ru/hackday/crabtrip/MyActivity.java
+++ trunk/src/ru/hackday/crabtrip/MyActivity.java
@@ -1,24 +1,43 @@
 package ru.hackday.crabtrip;
 
+import ru.hackday.crabtrip.view.CanvasView;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class MyActivity extends Activity {
+	
+	private Thread mOpsThread;
+	private CanvasView mView;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        new Thread(new Runnable() {
+        mView = (CanvasView) findViewById(R.id.gameView);
+    }
+    
+    @Override
+    protected void onResume() {
+    	mOpsThread =  new Thread(new Runnable() {
             public void run() {
-                while (true) {
-                    findViewById(R.id.gameView).post(new Runnable() {
-                        public void run() {
-                            findViewById(R.id.gameView).invalidate();
-                        }
-                    });
+                while (! Thread.interrupted()) {
+                	mView.postInvalidate();
+                	try {
+						Thread.sleep(50);
+					} catch (InterruptedException e) {
+						return;
+					}
                 }
             }
-        }).start();
+        }) ;
+    	mOpsThread.start();
+    	super.onResume();
+    }
+    
+    @Override
+    protected void onPause() {
+    	mOpsThread.interrupt();
+    	mOpsThread = null;
+    	super.onPause();
     }
 }
