415fb82111555fcb74fdfd224377bc12777d644b
ildar.karimov <ildar.karimov@a6d49d11-235e-086e-d3b0-479171f3bf0b>
2011-05-28 14:43:32 +0000

    endless loop with 1 point

--- trunk/src/ru/hackday/crabtrip/MyActivity.java
+++ trunk/src/ru/hackday/crabtrip/MyActivity.java
@@ -3,13 +3,22 @@ package ru.hackday.crabtrip;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MyActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class MyActivity extends Activity {
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (true) {
+                    findViewById(R.id.gameView).post(new Runnable() {
+                        public void run() {
+                            findViewById(R.id.gameView).invalidate();
+                        }
+                    });
+                }
+            }
+        }).start();
     }
 }
